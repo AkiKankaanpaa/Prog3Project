07b620eaaf4425085e5a0f28311f7cffe483bd3e
Samuli Käppi <samuli.kappi@tuni.fi>
2020-11-25 20:56:31 +0200

    Removed function that will probably never be used

--- Game/actor.h
+++ Game/actor.h
@@ -14,7 +14,6 @@ public:
     Actor(QGraphicsRectItem* gametoken, std::map<int, std::vector<int>>* coordinates);
     bool can_move(direction dir);
     std::pair<int, int> return_coordinates();
-    std::map<int, std::vector<int>>& return_map();
 
 public slots:
     void move(direction dir);
